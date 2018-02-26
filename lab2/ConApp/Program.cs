6e56acc00bae7d315a86f29b3416d71e30a1d3c2
sauron <vladimir.chernokulsky@gmail.com>
2018-02-27 01:32:31 +0300

    shape collection creation

--- lab2/ConApp/Program.cs
+++ lab2/ConApp/Program.cs
@@ -1,16 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Shapes;
+using ConApp.Factories;
+using System.Linq;
 
 namespace ConApp
 {
     class Program
     {
+        private const int SHAPE_COUNT = 5;
+
+        private static Random _random = new Random(DateTime.Now.Millisecond);
+
+
+        static void CreateShapes(ref IList<Shape> shapes)
+        {
+            for (int i = 0; i < SHAPE_COUNT; i++)
+            {
+                int x = _random.Next();
+                int y = _random.Next();
+
+                bool flag = _random.Next() % 2 == 0 ? true : false;
+                if (flag)
+                    shapes.Add(new Ellipse(x, y, _random.Next() % 40, _random.Next() % 40));
+                else
+                    shapes.Add(new Circle(x, y, _random.Next() % 40));
+            }
+        }
+
+        static void CreateShapesByFactory(ref IList<Shape> shapes)
+        {
+            var factories = new List<ShapeFactory>(new ShapeFactory[]
+                {
+                    new EllipseFactory(),
+                    new CircleFactory()
+                });
+            for (int i = 0; i < SHAPE_COUNT; i++)
+            {
+                int factoryIdx = _random.Next() % factories.Count;
+
+                int x = _random.Next();
+                int y = _random.Next();
+                shapes.Add(factories[factoryIdx].CreateShape(x, y));
+            }
+        }
+
         static void Main(string[] args)
         {
-            List<Shape> _shapes = new List<Shape>();
+            IList<Shape> shapes = new List<Shape>();
+            //CreateShapes(ref shapes);
+            CreateShapesByFactory(ref shapes);
+
+            foreach(var shape in shapes)
+            {
+                Console.Write("Object: {0}  ", shape.GetType());
+                Console.WriteLine(shape.GetArea());
+            }
+
+            var shapes1 = shapes.Select(x => x.GetArea() < 20);
+
+            Console.ReadKey();
         }
     }
 }
--- lab2/Shapes/Circle.cs
+++ lab2/Shapes/Circle.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Drawing;
-
-namespace Shapes
+﻿namespace Shapes
 {
     public class Circle : Ellipse
     {
