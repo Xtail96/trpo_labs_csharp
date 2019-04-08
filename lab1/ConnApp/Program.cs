1f189dd7e4a422c51539f2cdf8705ef7405d0d92
Xtail1996 <xtail1996@yandex.ru>
2019-04-08 09:28:59 +0300

    add rectangle (lab1)

--- lab1/ConnApp/Program.cs
+++ lab1/ConnApp/Program.cs
@@ -7,17 +7,16 @@ namespace ConnApp
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Hello world!!!");
-            Console.ReadKey(); 
-            */
+            //Console.WriteLine("Hello world!!!");
+            //Console.ReadKey(); 
 
-            /*
+            Rectangle rect = new Rectangle(0, 0, 10, 5);
+            rect.Show();
+
             Ellipse ellipse = new Ellipse(10, 10, 100, 50);
             Circle circle = new Circle(20, 20, 50);
             ellipse.Show();
             circle.Show();
-            */
         }
     }
 }
---
+++ lab1/Shapes/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Shapes
+{
+    public class Rectangle
+    {
+        protected Location _location;  // поле доступное в классе наследнике
+        private int _a;
+        private int _b;
+
+        public Rectangle(int x, int y, int a, int b)
+        {
+            this._location = new Location() { X = x, Y = y };
+            this._a = a;
+            this._b = b;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Class: Rectangle | Location: ({0},{1}) | Sides(a, b): ({2}, {3})", this._location.X, this._location.Y, this._a, this._b);
+        }
+    }
+}
